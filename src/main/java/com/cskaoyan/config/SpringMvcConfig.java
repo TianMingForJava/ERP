7b10a1c2fab8a081e24045b7a7bed2b232a36630
TianMingForJava <sunbw@163.com>
2019-05-17 11:06:39 +0800

    Initial commit

---
+++ src/main/java/com/cskaoyan/config/SpringMvcConfig.java
@@ -0,0 +1,12 @@
+package com.cskaoyan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "com.cskaoyan.controller")
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+}
